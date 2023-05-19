b90976e6e9dc0d86193eeb06db44070cc6889b0f
Tien Nguyen <17130244@st.hcmuaf.edu.vn>
2023-05-19 22:19:27 +0700

    completed styling header, footer, product category, newletter view for each screens (19/5/2023)

--- src/containers/customerView/Homepage/ProductCategory.js
+++ src/containers/customerView/Homepage/ProductCategory.js
@@ -29,52 +29,56 @@ class ProductCategory extends Component {
         return (
             <div className='product-category-container'>
                 <div className='product-category-content'>
-                    <div className='content-title'>
+                    <div className='content-title row'>
                         <div className='content-title-icon'></div>
                         <div className='content-title-text'>
-                            Product Category
+                            <FormattedMessage id="customer.homepage.product-category.title" />
                         </div>
                     </div>
-                    <div className='context-items'>
-                        <div className='child-item'>
+                    <div className='context-items row'>
+                        <div className='child-item col-1'>
                             <div className='item-image item1'></div>
                             <div className='item-text text-center'>VPP<br />DCHS</div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item2'></div>
-                            <div className='item-text text-center'>Đồ chơi</div>
+                            <div className='item-text text-center'><FormattedMessage id="customer.homepage.product-category.toys" /></div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item3'></div>
-                            <div className='item-text text-center'>Ngôn Tình<br />Đam Mỹ</div>
+                            <div className='item-text text-center'>
+                                <FormattedMessage id="customer.homepage.product-category.romantic-fiction" />
+                                <br />
+                                <FormattedMessage id="customer.homepage.product-category.danmei" />
+                            </div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item4'></div>
-                            <div className='item-text text-center'>Sách<br />Ngoại Ngữ</div>
+                            <div className='item-text text-center'><FormattedMessage id="customer.homepage.product-category.foreign-language-book" /></div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item5'></div>
                             <div className='item-text text-center'>Manga</div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item6'></div>
                             <div className='item-text text-center'>Tâm Linh<br />Luân Hồi</div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item7'></div>
                             <div className='item-text text-center'>Tâm Lý<br />Thao Túng</div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item8'></div>
                             <div className='item-text text-center'>Đối Mặt<br />Thức Tỉnh</div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item9'></div>
-                            <div className='item-text text-center'>Tiểu Thuyết</div>
+                            <div className='item-text text-center'><FormattedMessage id="customer.homepage.product-category.novel" /></div>
                         </div>
-                        <div className='child-item'>
+                        <div className='child-item col-1'>
                             <div className='item-image item10'></div>
-                            <div className='item-text text-center'>Light Novel</div>
+                            <div className='item-text text-center'><FormattedMessage id="customer.homepage.product-category.light-novel" /></div>
                         </div>
                     </div>
                 </div>
--- src/containers/customerView/components/Header.js
+++ src/containers/customerView/components/Header.js
@@ -26,7 +26,7 @@ class Header extends Component {
                     value: 'en', label: (
                         <>
                             <img src={enFlag}
-                                className='language-icon'
+                                className='language-icon '
                                 alt="English Logo" /> EN
                         </>
                     )
@@ -67,6 +67,7 @@ class Header extends Component {
         let customStyles = {
             control: (baseStyles, state) => ({
                 ...baseStyles,
+                width: 65,
                 boxShadow: "none",
                 cursor: "pointer",
                 "&:hover": {
@@ -76,35 +77,33 @@ class Header extends Component {
 
             dropdownIndicator: base => ({
                 ...base,
-                padding: 0,
+                padding: 1,
             }),
 
             option: (baseStyles, state) => ({
                 ...baseStyles,
-                padding: 8,
+                padding: 4,
                 cursor: "pointer"
             })
         };
 
         return (
-            <div className='home-header-container'>
+            <div className='home-header-container container-fluid'>
                 <div className='row'>
+                    {/* LOGO */}
+                    <div className='home-header-logo col-12 col-lg-2'>
 
-                    <div className='home-header-logo-menu col-md-3 col-12'>
-                        <div className='home-header-logo'>
-
-                        </div >
-
-                        <div className='home-header-menu'>
-                            <div className='home-header-menu-toggle'>
-                                <div className='home-header-menu-toggle-icon'></div>
-
-                                <i className="fa fa-angle-down"></i>
-                            </div>
-                        </div >
                     </div >
+                    {/* MENU */}
+                    <div className='home-header-menu col-1 col-lg-1 px-1'>
+                        <div className='home-header-menu-toggle'>
+                            <div className='home-header-menu-toggle-icon img-fluid'></div>
 
-                    <div className='home-header-search-bar col-md-6'>
+                            <i className="fa fa-angle-down"></i>
+                        </div>
+                    </div >
+                    {/* SEARCH BAR */}
+                    <div className='home-header-search-bar col-8 col-md-9 col-lg-5'>
                         <input type='text' className='form-control search-bar'
                             placeholder={
                                 language === languages.VI ?
@@ -113,28 +112,28 @@ class Header extends Component {
                             } />
                         <button><i className="fa fa-search"></i></button>
                     </div >
-
-                    <div className='home-header-user-options col-md-3'>
+                    {/* OPTIONS */}
+                    <div className='home-header-user-options col-3 col-md-2 col-lg-4'>
                         <div className='container'>
                             <div className='row'>
-                                <div className='user-options-notifications option col-3'>
+                                <div className='user-options-notifications option col-md-3'>
                                     <i className="fa fa-bell"></i>
                                     <p>
                                         <FormattedMessage id="customer.homepage.header.notifications" />
                                     </p>
                                 </div>
 
-                                <div className='user-options-cart option col-3'>
+                                <div className='user-options-cart option col-6 col-md-3'>
                                     <i className="fa fa-shopping-cart"></i>
                                     <p><FormattedMessage id="customer.homepage.header.cart" /></p>
                                 </div>
 
-                                <div className='user-options-information option col-3'>
+                                <div className='user-options-information option col-6 col-md-3'>
                                     <i className="fa fa-user"></i>
                                     <p><FormattedMessage id="customer.homepage.header.account" /></p>
                                 </div>
 
-                                <div className='user-options-change-language option col-3'>
+                                <div className='user-options-change-language option col-md-3'>
                                     <Select
                                         styles={customStyles}
                                         value={selectedLanguage}
