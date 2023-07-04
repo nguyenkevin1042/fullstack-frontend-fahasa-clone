9b34348bd9928e6ccd0c7c9eb43c6b1610bab422
Tien Nguyen <17130244@st.hcmuaf.edu.vn>
2023-07-04 15:35:25 +0700

    completed view for review product including responsive (4/7/2023)

--- src/containers/customerView/ProductDetail/ProductReviewComponent.js
+++ src/containers/customerView/ProductDetail/ProductReviewComponent.js
@@ -4,6 +4,8 @@ import { FormattedMessage } from 'react-intl';
 import { withRouter } from 'react-router';
 import './ProductReviewComponent.scss';
 import starIconGray from '../../../assets/images/ico_star_gray.svg'
+import starIconYellow from '../../../assets/images/ico_star_yellow.svg'
+
 // import * as actions from "../store/actions";
 
 import { Progress } from 'antd';
@@ -51,6 +53,7 @@ class ProductReviewComponent extends Component {
                             </p>
                             <p>(2 đánh giá)</p>
                         </div>
+
                         <div className='each-star-rating col-8 col-lg-4'>
                             <ul >
                                 <li className='sharing-star-rating row'>
@@ -76,12 +79,48 @@ class ProductReviewComponent extends Component {
                             </ul>
 
                         </div>
+
                         <div className='write-review-section col-12 col-lg-6'>
                             {isLoggedIn === true ?
-                                <></> :
+                                <button>
+                                    <i className="fas fa-pencil-alt"></i>
+                                    <FormattedMessage id="customer.product-detail.write-a-review" />
+                                </button> :
                                 <p>
                                     Chỉ có thành viên mới có thể viết nhận xét.Vui lòng đăng nhập hoặc đăng ký.
                                 </p>}
+                        </div>
+
+                        <div className='list-reviews-section container-fluid'>
+                            <div className='customer-review-item row'>
+                                <div className='customer-name-review-date d-flex d-lg-block col-12 col-lg-2'>
+                                    <p className='customer-name col-6 col-lg-12'>Bùi Minh Phượng</p>
+                                    <p className='review-date col-6 col-lg-12'>20/4/2023</p>
+                                </div>
+                                <div className='customer-review-rating-text d-block col-12 col-lg-10'>
+                                    <div className='stars-icon'>
+                                        <img src={starIconYellow} alt='star' />
+                                        <img src={starIconYellow} alt='star' />
+                                        <img src={starIconYellow} alt='star' />
+                                        <img src={starIconYellow} alt='star' />
+                                        <img src={starIconYellow} alt='star' />
+                                    </div>
+                                    <div className='customer-review-text'>
+                                        Balo là một vật dụng không thể thiếu của học sinh và dường như cứ đến đầu năm học là phụ huynh lại đau đầu mua balo cho bé. Balo xinh xắn , màu sắc dễ thương, thiết kế đơn giản cho bé, các bé gái không ai không mê những mẫu balo thế này. Mua hàng ở Fahasa thì cực kì yên tâm, giá cả đúng với chất lượng, hơn hết chiếc balo thần thánh này còn giúp các bé chống gù lưng. Mình hay đi đón em và rất hay thấy hình ảnh những em bé vẫn còn rất nhỏ nhưng trên lưng thì vác những cặp rất to, trông các bé rất cực nhọc và lâu dần , việc lưng gù xuống sẽ là không kì lạ. Các phụ huynh cần có lựa chọn thông minh cho các bé , vì sự phát triển sau này của các con. Lựa chọn một sản phẩm vừa dễ thương vừa tốt cho lưng của các bé thì còn gì bằng nữa ạ.
+                                    </div>
+                                </div>
+                                <div className='like-report col-12 d-flex'>
+                                    <p>
+                                        <i className="fa fa-thumbs-up" aria-hidden="true"></i>
+                                        Thích
+                                    </p>
+                                    <p>
+                                        <i className="fa fa-flag" aria-hidden="true"></i>
+                                        Báo cáo
+                                    </p>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
 
